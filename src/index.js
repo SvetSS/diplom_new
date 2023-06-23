8e42faf64a169d6e17cbdb0c62fb03b3d5993d10
Svet_lana_17 <ocelot_17@mail.ru>
2023-06-23 22:00:13 +0600

    добавление кода

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const modal = require('./modules/modal');
 const slider = require('./modules/slider');
 const sendForm = require('./modules/sendForm');
+const accordeon = require('./modules/accordeon');
 
 modal();
 slider();
 sendForm();
+accordeon();
