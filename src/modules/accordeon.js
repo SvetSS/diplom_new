97061c4e01840d0fa47f9b531a01033985c4d262
Svet_lana_17 <ocelot_17@mail.ru>
2023-06-24 00:21:30 +0600

    реализация акордеона

--- src/modules/accordeon.js
+++ src/modules/accordeon.js
@@ -1,90 +1,26 @@
 const accordeon = () => {
-    console.log('ggg')
+
     const accordeonBlock = document.querySelector('.accordeon');
-    const slides = accordeonBlock.querySelectorAll('.item ');
-    const tables = accordeonBlock.querySelectorAll('.table ');
+    const accordeons = accordeonBlock.querySelectorAll('.element ');
+    const elementContents = accordeonBlock.querySelectorAll('.element-content ');
 
-    let dotsList = document.createElement("ul");
-
-    let currentSlide = 0;
-    let interval;
-
-    sliderBlock.append(dotsList);
-
-    const nextSlide = (elems, index) => {
-        elems[index].style.display = '';
-    };
-    const nextElement = (elems, index, strClass) => {
-        elems[index].classList.add(strClass);
-    };
-    const prevSlide = (elems, index) => {
-        elems[index].style.display = 'none';
-    };
-    const prevElement = (elems, index, strClass) => {
-        elems[index].classList.remove(strClass);
-    };
-
-
-    dotsList.classList.add('slick-dots');
-
-    slides.forEach((item, idx) => {
-        const dot = document.createElement("li");
-        dot.classList.add('dot');
-        if (idx === 0) {
-            dot.classList.add('slick-active');
-        }
-
-        dotsList.append(dot);
-    });
-
-    tables.forEach((item, idx) => {
-        if (idx === 0) {
-            item.classList.add('active');
-        }
-    });
-
-    const dots = dotsList.querySelectorAll('.dot');
-    const autoSlide = () => {
-        prevSlide(slides, currentSlide);
-        prevElement(dots, currentSlide, 'slick-active');
-        prevElement(tables, currentSlide, 'active');
-        currentSlide++;
-
-        if (currentSlide >= slides.length) {
-            currentSlide = 0;
-        }
-
-        nextSlide(slides, currentSlide);
-        nextElement(dots, currentSlide, 'slick-active');
-        nextElement(tables, currentSlide, 'active');
-    };
-
-    const startSlide = (timer = 3000) => {
-        interval = setInterval(autoSlide, timer);
-    };
-
-
-    sliderBlock.addEventListener('click', e => {
+    accordeonBlock.addEventListener('click', e => {
         e.preventDefault();
-        if (!e.target.matches('.dot')) {
-            return;
-        }
 
-        prevSlide(slides, currentSlide, 'portfolio-item-active');
-        prevElement(dots, currentSlide, 'slick-active');
-        prevElement(tables, currentSlide, 'active');
-        if (e.target.classList.contains('dot')) {
-            dots.forEach((dot, index) => {
-                if (e.target === dot) {
-                    currentSlide = index;
+        if (e.target.classList.contains('element title') || e.target.closest('.element')) {
+            const tabBtn = e.target.closest('.element');
+            accordeons.forEach((item, index) => {
+                if (item === tabBtn) {
+                    console.log('ggg');
+                    accordeons[index].classList.add('active');
+                    elementContents[index].style.display = 'block';
+                } else {
+                    item.classList.remove('active');
+                    elementContents[index].style.display = 'none';
                 }
             });
         }
 
-
-        nextSlide(slides, currentSlide, 'portfolio-item-active');
-        nextElement(dots, currentSlide, 'slick-active');
-        nextElement(tables, currentSlide, 'active');
     });
 
 
